453130d19456cc86886f6a3561d8427cce13c749
Abisoye Akinloye <abisoyeakinloye@gmail.com>
2022-08-19 16:50:36 +0100

    ROS 2 C++ publisher

---
+++ Week 3/04. Topics/ros2_ws/build/pkg_cpp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/loye/Desktop/Projects/ROS-100-Days-Coding-Challenge/Week 3/04. Topics/ros2_ws/src/pkg_cpp
+# Build directory: /home/loye/Desktop/Projects/ROS-100-Days-Coding-Challenge/Week 3/04. Topics/ros2_ws/build/pkg_cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
