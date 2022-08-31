393a2ab49eea51026338ab0e4d812a9571a50d03
Abisoye Akinloye <abisoyeakinloye@gmail.com>
2022-08-31 12:21:57 +0100

    oop client cpp

--- Week 5/1. Service/ros2_ws/src/pkg_cpp_py/src/add_two_ints_oop_client.cpp
+++ Week 5/1. Service/ros2_ws/src/pkg_cpp_py/src/add_two_ints_oop_client.cpp
@@ -6,31 +6,32 @@ using namespace std::chrono_literals;
 class AddTwoIntsOOP : public rclcpp::Node
 {
 public:
-    AddTwoIntsOOP() : Node("add_two_ints_oop_client")
+    AddTwoIntsOOP(int arg1, int arg2) : Node("add_two_ints_oop_client")
     {
-        client_ = this->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+        RCLCPP_INFO(this->get_logger(), "Client ready to send request..");
 
-        while (!client_->wait_for_service(1s))
-        {
-            if (!rclcpp::ok())
-            {
-                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-            }
-            RCLCPP_WARN(this->get_logger(), "Server is not ready, waiting...");
-        }
+        thread = std::thread(std::bind(&AddTwoIntsOOP::callAddTwoIntsService, this, arg1, arg2));
     }
 
-    void send_request(int a = 3, int b = 4)
+    void callAddTwoIntsService(int a, int b)
     {
+        client = this->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+
+        while (!client->wait_for_service(1s))
+        {
+            RCLCPP_WARN(this->get_logger(), "Service not available, waiting again...");
+        }
+
+        auto request = example_interfaces::srv::AddTwoInts::Request::SharedPtr();
         request->a = a;
         request->b = b;
 
-        auto future = client_->async_send_request(request);
+        auto future = client->async_send_request(request);
 
         try
         {
             auto response = future.get();
-            RCLCPP_INFO(this->get_logger(), "The sum of %d and %d is %d", a, b, response->sum);
+            RCLCPP_INFO(this->get_logger(), "The sum of %d and %d is %d", a,b,response->sum);
         }
         catch (const std::exception &e)
         {
@@ -39,14 +40,15 @@ public:
     }
 
 private:
-    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client_;
-    example_interfaces::srv::AddTwoInts::Request::SharedPtr request;
+    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client;
+    std::thread thread;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<AddTwoIntsOOP>();
+    auto node = std::make_shared<AddTwoIntsOOP>(atoll(argv[1]),atoll(argv[2]));
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
