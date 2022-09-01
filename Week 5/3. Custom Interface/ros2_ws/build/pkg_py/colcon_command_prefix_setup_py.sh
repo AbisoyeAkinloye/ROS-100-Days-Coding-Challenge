5d73ff9a507a76d47a649626bddf4232c3af439f
Abisoye Akinloye <abisoyeakinloye@gmail.com>
2022-09-01 18:41:51 +0100

    hardware status pub

---
+++ Week 5/3. Custom Interface/ros2_ws/build/pkg_py/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/loye/Desktop/Projects/ROS-100-Days-Coding-Challenge/Week 5/3. Custom Interface/ros2_ws/install/custom_interfaces/share/custom_interfaces/package.sh"
--- Week 5/3. Custom Interface/ros2_ws/src/pkg_py/setup.py
+++ Week 5/3. Custom Interface/ros2_ws/src/pkg_py/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "hardware_pub=pkg_py.hardware_status_pub:main"
         ],
     },
 )
