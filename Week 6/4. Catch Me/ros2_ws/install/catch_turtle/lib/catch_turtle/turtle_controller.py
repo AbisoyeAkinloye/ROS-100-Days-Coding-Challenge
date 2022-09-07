2ff0d8c169f575065c080bf50c79b8464493b733
Abisoye Akinloye <abisoyeakinloye@gmail.com>
2022-09-07 11:12:00 +0100

    resolve spiral movement to goal

--- Week 6/4. Catch Me/ros2_ws/install/catch_turtle/lib/catch_turtle/turtle_controller.py
+++ Week 6/4. Catch Me/ros2_ws/install/catch_turtle/lib/catch_turtle/turtle_controller.py
@@ -11,8 +11,9 @@ class TurtleController(Node):
     def __init__(self):
         super().__init__("turtle_controller")
         self.pose = None
-        self.target_x = 8.0
-        self.target_y = 4.0
+        self.target_x = 4.0
+        self.target_y = 8.0
+
         self.cmd_vel_publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.pose_subscriber_ = self.create_subscription(Pose, "/turtle1/pose", self.turtle_pose_cb, 10)
         self.control_loop_timer_ = self.create_timer(0.01, self.control_loop)
@@ -32,15 +33,15 @@ class TurtleController(Node):
         
         if distance > 0.5:
             # position
-            cmd_vel.linear.x = 2*distance
+            cmd_vel.linear.x = distance
             # orientation
             goal_theta = math.atan2(dist_y,dist_x)
             diff = goal_theta - self.pose.theta
-            if diff > math.pi:
-                diff -= 2*math.pi
-            else:
-                diff += 2*math.pi
-            cmd_vel.angular.z = 6*diff
+            # if diff > math.pi:
+            #     diff -= 2*math.pi
+            # else:
+            #     diff += 2*math.pi
+            cmd_vel.angular.z = diff
         else:
             cmd_vel.linear.x = 0.0
             cmd_vel.angular.z = 0.0
